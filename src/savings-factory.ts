61f675eacd14d66ad2ab8b830a4cbc17aa26c0f8
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-09-23 03:48:58 -0400

    Added create savings subgraph

--- src/savings-factory.ts
+++ src/savings-factory.ts
@@ -1,18 +1,24 @@
 import { SavingsCreated as SavingsCreatedEvent } from "../generated/SavingsFactory/SavingsFactory"
-import { SavingsCreated } from "../generated/schema"
+import { Saving, TokenDistribution } from "../generated/schema"
 
 export function handleSavingsCreated(event: SavingsCreatedEvent): void {
-  let entity = new SavingsCreated(
+  let entity = new Saving(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.creator = event.params.creator
   entity.savings = event.params.savings
   entity.whitelistTokens = event.params.whitelistTokens
-  entity.tokenDistribution = event.params.tokenDistribution
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  for (let x = 0; x < event.params.tokenDistribution.length; x++) {
+    let tokenDistribution = new TokenDistribution(
+      event.transaction.hash.concatI32(x)
+    )
+    tokenDistribution.savings = event.params.savings
+    tokenDistribution.token = event.params.tokenDistribution[x].token
+    tokenDistribution.distribution =
+      event.params.tokenDistribution[x].distribution
+    tokenDistribution.save()
+  }
 
   entity.save()
 }
